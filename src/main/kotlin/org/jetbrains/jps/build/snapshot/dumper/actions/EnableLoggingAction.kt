7efce9debf0483ab82225acfb949e3124f22a333
Aleksei Cherepanov <Aleksei.Cherepanov@jetbrains.com>
2020-10-05 19:30:03 +0300

    Add "Enable Reset Logs and Enable JPS Build Logging" action

--- src/main/kotlin/org/jetbrains/jps/build/snapshot/dumper/actions/EnableLoggingAction.kt
+++ src/main/kotlin/org/jetbrains/jps/build/snapshot/dumper/actions/EnableLoggingAction.kt
@@ -5,20 +5,29 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.util.io.FileUtil
 import org.jetbrains.jps.cmdline.LogSetup
-import java.io.BufferedOutputStream
-import java.io.File
-import java.io.FileOutputStream
+import java.io.*
 import java.util.*
 
 class EnableLoggingAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val logDirectory = BuildManager.getBuildLogDirectory()
+        val buildLogProperties = File(logDirectory, LogSetup.LOG_CONFIG_FILE_NAME)
+        val config: InputStream
+        if(buildLogProperties.exists()) {
+            config = FileInputStream(buildLogProperties)
+        } else {
+            config = LogSetup.readDefaultLogConfig()
+        }
+
+        val properties = Properties().also {
+            it.load(config)
+            it.setProperty("log4j.logger.org.jetbrains.jps", "debug")
+            it.setProperty("log4j.logger.#org.jetbrains.jps", "debug")
+        }
+
         FileUtil.delete(logDirectory)
         FileUtil.createDirectory(logDirectory)
-        val properties = Properties()
-        LogSetup.readDefaultLogConfig().use { config -> properties.load(config) }
-        properties.setProperty("log4j.rootLogger", "debug, file")
-        val logFile = File(logDirectory, LogSetup.LOG_CONFIG_FILE_NAME)
-        BufferedOutputStream(FileOutputStream(logFile)).use { output -> properties.store(output, null) }
+
+        BufferedOutputStream(FileOutputStream(buildLogProperties)).use { output -> properties.store(output, null) }
     }
 }
